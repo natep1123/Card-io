3529b83677f119f583a15709938597c8666a7578
Nate <nathaniel.perry1123@gmail.com>
2025-05-19 21:44:35 -0400

    Removed apostrophes in about page causing deployment error

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
       <h2>About Card-io</h2>
       <div className="text-left">
         <h3 className="text-lg font-semibold">How does it work?</h3>
-        <p>It's in development, so I'll let you know when I figure it out!</p>
+        <p>Card-io is in development.</p>
       </div>
     </main>
   );
