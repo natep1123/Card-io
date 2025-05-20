e535f6a39c83cbfac30c32de7bca1edb181abc8c
Nate <nathaniel.perry1123@gmail.com>
2025-05-20 11:17:13 -0400

    Added total time to summary

--- src/components/workout/WSummary.jsx
+++ src/components/workout/WSummary.jsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { useWorkoutContext } from "@/contexts/WorkoutContext";
+import { useEffect, useState } from "react";
 import WStats from "./tables/WStats";
 
 export default function WSummary() {
-  const { resetWorkout } = useWorkoutContext();
+  const { resetWorkout, clockStart, formatClock } = useWorkoutContext();
+  const [clock, setClock] = useState(null);
+
+  useEffect(() => {
+    const elapsed = Math.floor((Date.now() - clockStart) / 1000);
+    setClock(elapsed);
+  }, [clockStart]);
 
   function handleClick() {
     resetWorkout();
@@ -13,6 +20,9 @@ export default function WSummary() {
   return (
     <div className="w-full flex flex-col items-center">
       <h2 className="text-2xl font-bold mb-4">Workout Summary</h2>
+      <h3 className="text-lg">
+        Total Time: <span className="font-bold">{formatClock(clock)}</span>
+      </h3>
       <WStats />
       <button
         onClick={handleClick}
