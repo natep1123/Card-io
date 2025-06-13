ea6193798345242e7f91a6f74812e1f34eef8e6c
Nate <nathaniel.perry1123@gmail.com>
2025-06-13 16:45:14 -0400

    Updated profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -10,23 +10,9 @@ export default async function ProfilePage() {
         <h2 className="text-4xl font-bold mb-4">
           Only Users Can View Profiles.
         </h2>
-        <p className="text-lg text-gray-400 mb-6">
+        <p className="text-lg text-gray mb-6">
           Please log in and/or register an account to access this page.
         </p>
-        <div className="flex flex-col gap-4">
-          <Link
-            href="/login"
-            className="bg-red font-semibold rounded-md px-6 py-3 text-center hover:bg-red-600 transition"
-          >
-            Login
-          </Link>
-          <Link
-            href="/register"
-            className="bg-green font-semibold rounded-md px-6 py-3 text-center hover:bg-green-600 transition"
-          >
-            Register
-          </Link>
-        </div>
       </div>
     );
   }
